abfcf69264dce41d207ed871960e15e45f2f7599
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-05-27 18:12:27 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,12 +1,13 @@
-# 19-5-2018 MRC-Epid JHZ
+# 27-5-2018 MRC-Epid JHZ
 
 git add README.md
 git commit -m "README"
+git add 2367.zip jss2367.rar BLR.zip
 git commit -m "JSS paper"
-git add st.sh
-git commit -m "st.sh"
 git add GRM.grm.id
 git commit -m "GRM.grm.id"
 git add GRM.grm-*.gz
 git commit -m "GRM.grm.gz"
+git add st.sh
+git commit -m "st.sh"
 git push
