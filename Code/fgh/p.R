9e24cd9d817a67e612ac0932efdc987073b78e7c
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-06-07 09:40:58 +0100

    relocated

--- Code/fgh/p.R
+++
@@ -1,8 +0,0 @@
-pheno <- read.table("p.dat", col.names = c("pid", "id", "r"))
-N <- nrow(pheno)
-trios <- read.table("trios.dat", header = TRUE)
-is.na(trios[trios == 0]) <- TRUE
-f <- merge(pheno, trios[, -1], by = "id", all = TRUE)
-p <- data.frame(f[with(f, order(pid, id)), ], u = seq_len(N), e = seq_len(N))
-rownames(p) <- seq_len(N)
-save(p, file = "p.rda")
--- Code/gcta/gcta.sh
+++
@@ -1,4 +0,0 @@
-# 24-4-2018 JHZ
-
-gcta64 --bfile test --make-grm-bin --out test
-
