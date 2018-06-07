308a0b8eac4e59a740dfddefad40fedc61d70584
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-06-07 09:38:36 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,15 +1,13 @@
-# 4-6-2018 MRC-Epid JHZ
+# 7-6-2018 MRC-Epid JHZ
 
 git add README.md
 git commit -m "README"
-git add paper.* Figures Code ref.bib jss2367.rar
-git commit -m "JSS paper"
-git add BLR.zip boot.zip
-git commit -m "GCTA documentation example"
-git add GRM.grm.id
-git commit -m "GRM.grm.id"
-git add GRM.grm-*.gz
-git commit -m "GRM.grm.gz"
+git add JSS
+git commit -m "J Stat Soft paper"
+git add JPS
+git commit -m "J Prob Stat paper"
+git add T2D
+git commit -m "GxE interaction on T2D"
 git add st.sh
 git commit -m "st.sh"
 git add T2D
