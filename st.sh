482b7acd677cc7789f5a2d0e6d9230f449379320
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-06-09 08:04:29 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,4 +1,4 @@
-# 7-6-2018 MRC-Epid JHZ
+# 9-6-2018 MRC-Epid JHZ
 
 git add README.md
 git commit -m "README"
@@ -7,9 +7,7 @@ git commit -m "J Stat Soft paper"
 git add JPS
 git commit -m "J Prob Stat paper"
 git add T2D
-git commit -m "GxE interaction on T2D"
+git commit -m "A T2D analysis"
 git add st.sh
 git commit -m "st.sh"
-git add T2D
-git commit -m "T2D analysis"
 git push
