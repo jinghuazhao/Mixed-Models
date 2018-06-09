31f1a16339d0e7c7cfc5f717acf1869123d0021a
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-06-09 08:09:49 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -6,8 +6,8 @@ git add JSS
 git commit -m "J Stat Soft paper"
 git add JPS
 git commit -m "J Prob Stat paper"
-git add T2D
-git commit -m "A T2D analysis"
+git add GxE
+git commit -m "A GxE analysis"
 git add st.sh
 git commit -m "st.sh"
 git push
